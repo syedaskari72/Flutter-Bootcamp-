5302177ab4f1114b9482b8667d823d621e1d75a3
syedaskari72 <88816921+syedaskari72@users.noreply.github.com>
2022-08-30 01:12:19 +0500

    first commit

---
+++ flutter_1/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'My App',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Home Page"),
+        ),
+        body: new Center(
+          child: new Text("Hello World"),
+        ),
+      ),
+    );
+  }
+}
