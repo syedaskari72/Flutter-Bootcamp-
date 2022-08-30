f21849a3936abdf9bf45c7be4813b182e10dfa76
syedaskari72 <88816921+syedaskari72@users.noreply.github.com>
2022-08-31 03:18:06 +0500

    login page

---
+++ login_page/lib/main.dart
@@ -0,0 +1,105 @@
+// ignore_for_file: unnecessary_new, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new loginPage(),
+      theme: new ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
+
+class loginPage extends StatefulWidget {
+  @override
+  State createState() => new loginPageState();
+}
+
+class loginPageState extends State<loginPage>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> _iconAnimation;
+  late AnimationController _iconAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _iconAnimationController = new AnimationController(
+        vsync: this, duration: new Duration(milliseconds: 500));
+    _iconAnimation = new CurvedAnimation(
+      parent: _iconAnimationController,
+      curve: Curves.easeInOutBack,
+    );
+    _iconAnimation.addListener(() => this.setState(() {}));
+    _iconAnimationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.black,
+      body: new Stack(fit: StackFit.expand, children: <Widget>[
+        new Image(
+          image: AssetImage("assets/askari.jpg"),
+          fit: BoxFit.cover,
+          color: Colors.black87,
+          colorBlendMode: BlendMode.darken,
+        ),
+        new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new FlutterLogo(
+              size: _iconAnimation.value * 100,
+            ),
+            new Form(
+              child: new Theme(
+                data: new ThemeData(
+                    brightness: Brightness.dark,
+                    primarySwatch: Colors.teal,
+                    inputDecorationTheme: new InputDecorationTheme(
+                        labelStyle:
+                            new TextStyle(color: Colors.teal, fontSize: 20.0))),
+                child: Container(
+                  padding: const EdgeInsets.all(50.0),
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      new TextFormField(
+                        decoration: new InputDecoration(
+                          labelText: "Enter Email",
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      new TextFormField(
+                        decoration: new InputDecoration(
+                          labelText: "Enter Password",
+                        ),
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                      ),
+                      new Padding(
+                        padding: const EdgeInsets.only(top: 40.0),
+                      ),
+                      new MaterialButton(
+                        height: 40.0,
+                        minWidth: 100.0,
+                        color: Colors.teal,
+                        textColor: Colors.white,
+                        child: new Text("Login "),
+                        onPressed: () => {},
+                        splashColor: Colors.blueAccent,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        )
+      ]),
+    );
+  }
+}
